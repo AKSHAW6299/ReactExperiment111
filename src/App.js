5548b5b865d189991f58a85b13ddce900ef7fdfe
Anand6262 <pyexpert62@gmail.com>
2025-12-12 16:30:08 +0530

    çhanegs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Parent from './views/Parent';
 import Child from './views/Child';
-import GrandChild from './views/GrandChild';
 
 function App() {
   
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Parent />} />
         <Route path="/child" element={<Child />} />
-        <Route path="/grand-child" element={<GrandChild />} />
       </Routes>
     </Router>
   )
